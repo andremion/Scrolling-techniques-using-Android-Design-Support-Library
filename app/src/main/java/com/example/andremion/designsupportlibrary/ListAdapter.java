28218886721092ac32cc8c11a9366da86ac63be0
André Mion <andremion@gmail.com>
2015-08-18 19:12:38 -0300

    Refactor on item click listener

--- app/src/main/java/com/example/andremion/designsupportlibrary/ListAdapter.java
+++ app/src/main/java/com/example/andremion/designsupportlibrary/ListAdapter.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import java.util.List;
 
-public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
+public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> implements View.OnClickListener {
 
     private final List<String> mItems;
     private OnItemClickListener mItemClickListener;
@@ -25,25 +25,26 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(android.R.layout.simple_list_item_1, parent, false);
+        v.setOnClickListener(this);
         return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, final int position) {
+        holder.mItemView.setTag(position);
         holder.mTextView.setText(mItems.get(position));
-        holder.mTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mItemClickListener != null) {
-                    mItemClickListener.onItemClick(v, position);
-                }
-            }
-        });
     }
 
     @Override
     public int getItemCount() {
         return mItems.size();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (mItemClickListener != null) {
+            mItemClickListener.onItemClick(v, (Integer) v.getTag());
+        }
     }
 
     public interface OnItemClickListener {
@@ -52,10 +53,12 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder> {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
+        private final View mItemView;
         private final TextView mTextView;
 
         private ViewHolder(View v) {
             super(v);
+            mItemView = v;
             mTextView = (TextView) v.findViewById(android.R.id.text1);
         }
     }
